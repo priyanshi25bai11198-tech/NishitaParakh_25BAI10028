d980b7701ed9e89393b5ab34a87ea85b6fdc0ad8
NishitaParakh <parakhnishita14@gmail.com>
2026-08-30 22:24:56 +0530

    Complete RAG Pipeline with LLM Generation

--- rag.py
+++ rag.py
@@ -1,7 +1,15 @@
 import faiss
 import numpy as np
+import os
 from pypdf import PdfReader
 from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=groq_api_key)
+
 #------------------
 #1. DOCUMENT INGESTION
 #------------------
@@ -31,8 +39,6 @@ while st < len(text):
 #------------------
 embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 embeddings = embedding_model.encode(chunks)
-print("No. of embeddings: ", len(embeddings))
-print("Embedding size: ", len(embeddings[0]))
 
 #------------------
 #4. Vector Store(FAISS)
@@ -40,7 +46,6 @@ print("Embedding size: ", len(embeddings[0]))
 embeddings = np.array(embeddings).astype("float32")
 index = faiss.IndexFlatL2(embeddings.shape[1])
 index.add(embeddings)
-print("No. of vectors in FAISS: ", index.ntotal)
 
 #------------------
 #5. Retrieval
@@ -49,8 +54,40 @@ query = input("Ask a Question: ")
 query_embedding = embedding_model.encode([query])
 query_embedding = np.array(query_embedding).astype("float32")
 k = 3
+relevant_chunks = []
 distances, indices = index.search(query_embedding, k)
-print("\nRelevant Chunks\n")
 for i in indices[0]:
-    print(chunks[i])
-    print("------------------------")
+    relevant_chunks.append(chunks[i])
+
+#------------------
+#6. GENERATION(Using LLM)
+#------------------
+context = "\n\n".join(relevant_chunks)
+prompt = f"""
+Answer the question using the provided context.
+Context:
+{context}
+
+Question:
+{query}
+
+If the context contains relevant information, use it to answer.
+Do not use outside knowledge.
+
+If the context does not contain enough information to answer, say:
+"I could not find enough information in the provided document."
+"""
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-20b",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+answer = response.choices[0].message.content
+print("\nAnswer:")
+print(answer)
+
