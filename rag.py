af5a100bc966e55d97f00bdba5126aa3002d5fdf
NishitaParakh <parakhnishita14@gmail.com>
2026-08-30 21:30:28 +0530

    Added Similarity Search Retrieval

--- rag.py
+++ rag.py
@@ -41,3 +41,16 @@ embeddings = np.array(embeddings).astype("float32")
 index = faiss.IndexFlatL2(embeddings.shape[1])
 index.add(embeddings)
 print("No. of vectors in FAISS: ", index.ntotal)
+
+#------------------
+#5. Retrieval
+#------------------
+query = input("Ask a Question: ")
+query_embedding = embedding_model.encode([query])
+query_embedding = np.array(query_embedding).astype("float32")
+k = 3
+distances, indices = index.search(query_embedding, k)
+print("\nRelevant Chunks\n")
+for i in indices[0]:
+    print(chunks[i])
+    print("------------------------")
