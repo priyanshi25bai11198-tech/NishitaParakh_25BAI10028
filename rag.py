2dbc36da8a3fd210b6eb8563d004f737184cb2ee
NishitaParakh <parakhnishita14@gmail.com>
2026-08-30 12:41:16 +0530

    Add text Chunking

--- rag.py
+++ rag.py
@@ -1,7 +1,34 @@
+#------------------
+#DOCUMENT INGESTION
+#------------------
 from pypdf import PdfReader
 reader = PdfReader("document.pdf")
 text = ""
-for pages in reader.pages:
-    text += pages.extract_text()
+for page in reader.pages:
+    page_text = page.extract_text()
 
-print (text)
+    if page_text:
+        text += page_text + "\n"
+
+print(text)
+
+#------------------
+#CHUNKING
+#------------------
+chunk_size = 500
+overlap = 50
+chunks = []
+st = 0
+while st < len(text):
+    end = st + chunk_size
+    chunk = text[st:end]
+    chunks.append(chunk)
+    st = end - overlap
+
+print("Number of Chunks: ", len(chunks))
+print("first chunk: ", chunks[0])
+
+
+#------------------
+#CHUNKING
+#------------------
